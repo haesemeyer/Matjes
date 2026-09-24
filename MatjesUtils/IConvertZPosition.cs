311d09f1ff4c78b265d7a4787d5699972a815ba8
Martin Haesemeyer <m.haesemeyer@gmail.com>
2026-09-24 16:41:47 -0400

    Added interface for z-position converters

---
+++ MatjesUtils/IConvertZPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatjesUtils
+{
+    /// <summary>
+    /// Interface that needs to be implemented by calibration objects that relate
+    /// the Z-Position to our objective Piezo to the z-position of scan mirrors
+    /// </summary>
+    public interface IConvertZPosition
+    {
+        public abstract double ConvertPiezoToZ(double v_piezo);
+    }
+}
